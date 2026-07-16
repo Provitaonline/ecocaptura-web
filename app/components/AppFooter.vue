2b5847ee56850d1cb9aecc3f8a5344874f082caa
Ricardo Morin <morinricardo@gmail.com>
2026-07-16 12:07:45 -0700

    AppFooter

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+    <section>
+      <footer class="footer">
+        <div class="has-text-centered">
+         Here goes the footer
+        </div>
+      </footer>
+    </section>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,12 +4,6 @@
     <main>
       <slot />
     </main>
-    <section>
-      <footer class="footer">
-        <div class="has-text-centered">
-         Here goes the footer
-        </div>
-      </footer>
-    </section>
+    <AppFooter />
   </div>
 </template>
